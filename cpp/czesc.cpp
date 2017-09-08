900e078b6bc545fad8fc386c82e81e57c8100066
kl3ag1 <kl3ag1@komp02.lo1cg.org>
2017-09-08 07:49:53 +0200

    08,09

--- cpp/czesc.cpp
+++ cpp/czesc.cpp
@@ -11,11 +11,20 @@ using namespace std;
 #define ROK_TERAZ 2017
 #define ROK_CPP 1984
 
+int parzyste (int n) {
+    int i;
+    for (i=0; i<=n; i+=2)
+        cout << i << " ";
+    return i/2;
+    }
+
 int main(int argc, char **argv)
 {
     string a;
     int b;
     int rok_urodzenia;
+    int n;
+    
     cout << "Cześć. Jak się nazywasz " << endl;
     cin >> a;
     cout << "Cześć " << a << endl;
@@ -32,6 +41,10 @@ int main(int argc, char **argv)
         
     if (rok_urodzenia == ROK_CPP) {
         cout << "Jesteśmy tak samo starzy" << endl; }
+        
+    cout << "Podaj liczbę: " << endl;
+    cin >> n;
+    cout << "Parzystych: " << parzyste(n) << endl;
     
 	return 0;
 }
--- python/czesc.py
+++ python/czesc.py
@@ -7,12 +7,27 @@
 ROK_TERAZ = 2017
 ROK_PYTHON = 1991
 
+def parzyste():
+    ile = list(range(0, n+1, 2))
+    print(ile)
+    return len(ile)
+
 def main(args):
     imie = input("Jak się nazywasz? ")
     print ("Cześć", imie)
     wiek = int(input("Ile masz lat "))
-    rok_urodzenia
+    rok_urodzenia = ROK_TERAZ - wiek
     print ("Urodziłaś się w ", 2017 - wiek)
+    
+    if rok_urodzenia < ROK_PYTHON:
+        print("Jestem starszy")
+    elif rok_urodzenia > ROK_PYTHON:
+        print("Jestem młodszy")
+    else:
+        print("Jesteśmy tak samo starzy")
+        
+    n = int(input("Podaj liczbę: "))
+    print("Parzystych:", parzyste(n))
     
     return 0
 
