4d55d91e51063f48878c8def6ddf196ca53b4869
kl3ag1 <kl3ag1@komp02.lo1cg.org>
2017-10-10 10:55:48 +0200

    10,10

--- bazy/dane.py
+++ bazy/dane.py
@@ -6,20 +6,31 @@
 
 import csv
 
-def dane_z_pliku(plik):
+def dane_z_pliku(plik, delimiter='\t'): 
     dane = []
-    with open(plik, 'r') as plikcsv:
-        tresc = csv.reader(plikcsv, delimiter='\t')
+    with open(plik, 'r') as plikcsv: #otwiera, obsługuje i odrazu zamyka plik r - do odczytu
+        tresc = csv.reader(plikcsv, delimiter=delimiter) # treść - zmiena csv.reader - dane oddzielone jednym znakiem
     
-        for rekord in tresc:
+        for rekord in tresc: 
             dane.append(rekord)
             
-    print(dane)
+    return(dane)
+
+def wyczysc_dane(dane, pole):
+    for i, rekord in enumerate(dane):
+        el = rekord[pole]
+        el = el.replace('zł', '')
+        el = el.replace(' ', '')
+        el = el.replace(',', '.')
+        
+    return ''
+
 
 def main(args):
     #dane_z_pliku('premia.txt')
     #dane_z_pliku('dział.txt')
-    dane_z_pliku('pracownicy.txt')
+    dane = dane_z_pliku('pracownicy.txt')
+    pracownicy = wyczysc_dane(dane, 5)
     return 0
 
 if __name__ == '__main__':
