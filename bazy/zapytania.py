622e08c999dade87e7082fd5cdc517d86a489247
kl3ag1 <kl3ag1@komp02.lo1cg.org>
2017-10-17 11:24:25 +0200

    17,10

--- bazy/zapytania.py
+++ bazy/zapytania.py
@@ -5,32 +5,91 @@
 
 import sqlite3
 
-def kw_d(cur):
+def kw_g(cur):# gdy """ """ przygotowanie zapytania
     cur.execute(""" 
-        SELECT siedziba, 
-        FROM pracownicy, dzial
+        SELECT imie, nazwisko, stanowisko,
+        (JulianDay())
+        FROM pracownicy
     """)
+    
+    wyniki = cur.fetchall()  #feth - pobierz; fethall - pobierz wszystko
+    for row in wyniki:# row - zmienna
+        print(tuple(row)) # przekształć i wydrukuj
+
+def kw_f(cur):# gdy """ """ przygotowanie zapytania
+    kobiety = cur.execute(""" 
+        SELECT AVG (placa)
+        FROM pracownicy
+        WHERE imie LIKE('%a')
+    """)
+    
+    wyniki = kobiety.fetchall()  #feth - pobierz; fethall - pobierz wszystko
+    for row in wyniki:# row - zmienna
+        print(tuple(row)) # przekształć i wydrukuj
+    
+    
+    men = cur.execute(""" 
+        SELECT AVG (placa)
+        FROM pracownicy
+        WHERE imie NOT LIKE('%a')
+    """)
+        
+    wyniki = men.fetchall()  #feth - pobierz; fethall - pobierz wszystko
+    for row in wyniki:# row - zmienna
+        print(tuple(row)) # przekształć i wydrukuj
+
+def kw_e(cur):
+    cur.execute(""" 
+        SELECT nazwisko, stanowisko,
+        pracownicy.placa *        
+        (SELECT premia.premia
+        FROM premia
+        WHERE pracownicy.stanowisko = premia.id) 
+        AS premia
+        FROM pracownicy
+        ORDER BY premia DESC
+    """)
+    # (...) mechanizm podzapytań
+    wyniki = cur.fetchall()  #feth - pobierz; fethall - pobierz wszystko
+    for row in wyniki:# row - zmienna
+        print(tuple(row)) # przekształć i wydrukuj
+
+def kw_d(cur):
+    nazwa = input("Podaj nazwę działu: ")
+    siedziba = input("Podaj nazwę siedziba: ")
+    print(nazwa)
+    
+    cur.execute(""" 
+        SELECT nazwisko, imie, id_dzial, dzial.nazwa, dzial.siedziba
+        FROM pracownicy, dzial
+        WHERE pracownicy.id_dzial = dzial.id 
+        AND dzial.nazwa = ? 
+        AND siedziba = ? 
+    """, (nazwa, siedziba))
+    
+    wyniki = cur.fetchall()  #feth - pobierz; fethall - pobierz wszystko
+    for row in wyniki:# row - zmienna
+        print(tuple(row)) # przekształć i wydrukuj
 
 def kw_c(cur):# gdy """ """ przygotowanie zapytania
     cur.execute(""" 
         SELECT siedziba, SUM(placa) AS pensja
         FROM pracownicy, dzial
-        WHERE pracownicy.id_dzial=dzial.id
+        WHERE pracownicy.id_dzial = dzial.id
         GROUP BY siedziba
         ORDER BY pensja ASC
     """)
+    
     wyniki = cur.fetchall()  #feth - pobierz; fethall - pobierz wszystko
     for row in wyniki:# row - zmienna
         print(tuple(row)) # przekształć i wydrukuj
-        
-        
 
 def main(args):
     con = sqlite3.connect('pracownicy.sqlite3') # połączenie z bazą
     cur = con.cursor() #utworzenie kursora(gdy coś robię w bazie)
     con.row_factory = sqlite3.Row #możliwość odwoływania się do nazwy kolumn
     
-    kw_c(cur)
+    kw_g(cur)
     
     return 0
 
