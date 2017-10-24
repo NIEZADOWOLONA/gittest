8a1fab418c252d57b10bc8972e0061fb64bae44b
kl3ag1 <kl3ag1@komp02.lo1cg.org>
2017-10-24 11:32:52 +0200

    24,10

--- bazydarnych/sqlite/uczniowie_winiarczyk.py
+++ bazydarnych/sqlite/uczniowie_winiarczyk.py
@@ -1,46 +1,75 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  zapytania.py  
+#  zapytania.py
 
 import sqlite3
 
-def kw_a(cur):# gdy """ """ przygotowanie zapytania
-    cur.execute(""" 
+def kw_a(cur):
+    cur.execute("""
         SELECT Imie, Nazwisko, tbKlasy.Klasa
         FROM tbUczniowie, tbKlasy
         WHERE tbUczniowie.KlasaID = tbKlasy.IDKlasy
-        AND Klasa LIKE ('1A')
+        AND Klasa LIKE '1A'
     """)
     
-    wyniki = cur.fetchall()  #feth - pobierz; fethall - pobierz wszystko
-    for row in wyniki:# row - zmienna
-        print(tuple(row)) # przekształć i wydrukuj
+    wyniki = cur.fetchall()
+    for row in wyniki:
+        print(tuple(row))
 
-def kw_b(cur):# gdy """ """ przygotowanie zapytania
-    cur.execute(""" 
+def kw_b(cur):
+    cur.execute("""
         SELECT MAX(EgzHum)
         FROM tbUczniowie
-        """)
-        
-    wyniki = cur.fetchall()  #feth - pobierz; fethall - pobierz wszystko
-    for row in wyniki:# row - zmienna
-        print(tuple(row)) # przekształć i wydrukuj
+    """)
+    
+    wyniki = cur.fetchall()
+    for row in wyniki:
+        print(tuple(row))
+    
+def kw_c(cur):
+    cur.execute("""
+        SELECT AVG(EgzMat), tbKlasy.Klasa
+        FROM tbUczniowie, tbKlasy
+        WHERE tbUczniowie.KlasaID = tbKlasy.IDKlasy
+        AND Klasa LIKE '1A'
+    """)
+    
+    wyniki = cur.fetchall()
+    for row in wyniki:
+        print(tuple(row))
 
-def kw_c(cur):# gdy """ """ przygotowanie zapytania
-    cur.execute(""" 
-        SELECT AVG(EgzMat)
-        FROM tbUczniowie
-        """)
+def kw_d(cur):
+    cur.execute("""
+        SELECT Imie, Nazwisko, tbOceny.Ocena, tbPrzedmioty.Przedmiot
+        FROM tbUczniowie, tbOceny, tbPrzedmioty
+        WHERE tbOceny.UczenID = tbUczniowie.IDUcznia
+        AND tbOceny.PrzedmiotID = tbPrzedmioty.IDPrzedmiotu
+        AND Nazwisko LIKE 'Nowak'
+    """)
+
+    wyniki = cur.fetchall()
+    for row in wyniki:
+        print(tuple(row))
+
         
-    wyniki = cur.fetchall()  #feth - pobierz; fethall - pobierz wszystko
-    for row in wyniki:# row - zmienna
-        print(tuple(row)) # przekształć i wydrukuj
+def kw_e(cur):
+    cur.execute("""
+        SELECT Datad, tbPrzedmioty.Przedmiot, AVG(Ocena)
+        FROM tbOceny, tbPrzedmioty
+        WHERE tbOceny.PrzedmiotID = tbPrzedmioty.IDPrzedmiotu
+        AND strftime('%m',Datad) LIKE '10'
+        AND Przedmiot LIKE 'fizyka'
+    """)
+    
+    wyniki = cur.fetchall()
+    for row in wyniki:
+        print(tuple(row))
 
 def main(args):
-    con = sqlite3.connect('szkola.db') # połączenie z bazą
-    cur = con.cursor() #utworzenie kursora(gdy coś robię w bazie)
-    con.row_factory = sqlite3.Row #możliwość odwoływania się do nazwy kolumn
+    con = sqlite3.connect('szkola.db')
+    cur = con.cursor() 
+    con.row_factory = sqlite3.Row
     
     kw_c(cur)
     
